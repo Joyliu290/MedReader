3ae6e44863ba618f8fc1104701ecdf9329f66de4
Sahana <su575@nyu.edu>
2020-02-16 07:27:27 -0800

    integrated

--- mysite/treehacks/views.py
+++ mysite/treehacks/views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 import django.template.loader as loader
+from imageToText import detect_text
 import sys
 from .models import PhotoUploader
 from PIL import Image
@@ -19,14 +20,14 @@ def upload_image(request):
         image = PhotoUploader(name = uploaded_file.name, image = uploaded_file)
         image.save()
         print("image full path", image.image.path)
-        img = Image.open(image.image.path)
-        img.show() 
+        detect_text(image.image.path)
+        
         # 'document' is the input name in the html
         # the value of request.FILES['document'] is an object of the type UploadedFiles
 
         # perhaps look into using generator for this? since it will be a large number of text, and ideally don't want to use memory
         # to store it
         
-        return render(request, 'index.html')
+        return render(request, 'index3.html')
     else:
-        return render(request, "index.html")
+        return render(request, "index3.html")
